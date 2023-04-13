4cb03521d48c599e5573cacdb379f79bc3656130
Kirill Babaskin <99066434+RFKolor@users.noreply.github.com>
2023-04-13 16:01:14 +0300

    исправление ошибок(чтобы адекватно работала на 1.7.3)

--- Upserver-main.py
+++ Upserver-main.py
@@ -4,11 +4,10 @@ import random
 TOKEN = "MTAwNTU1NjAyNDcwODcxNDQ5Ng.G_rGMJ.Grl93vvL41--svtgGBhgBJSeJAeGkzr7bVleIA"
 
 bot = commands.Bot(command_prefix=('!'))
-check = 0
 @bot.event
 async def on_ready():
     print("Я запущен!")
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("DOKA 2"), afk=True)
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("wait your comand :D"), afk=True)
 
 @bot.command()
 async def lx(ctx, *text):
@@ -17,18 +16,9 @@ async def lx(ctx, *text):
 
 
 @bot.command()
-async def tl(ctx):
-    embed = discord.Embed(
-        title=">---жми---<",
-        description="ссылка на Tlauncher",
-        url='https://tlauncher.org/',
-    )
-    await ctx.send(embed=embed)
-
-@bot.command()
-async def start(ctx):
+async def report(ctx):
     user = await bot.fetch_user(user_id=575345431240769548)
-    await user.send('запусти сервер пж')
+    await user.send('bug report')
 
 @bot.command()
 async def hi(ctx):
@@ -41,36 +31,6 @@ async def flip(ctx):
         title=random.choice(coins),
     )
     await ctx.send(embed=embed)
-
-@bot.command()
-async def spam(ctx, text):
-    number = random.randint(0, 9)
-    while True:
-        await ctx.send("fuck")
-
-# код для спама рандомным людям
-@bot.command()
-# не используется на данный момент из-за ненадобности
-# async def id(ctx):
-    id = ""
-    count = 0
-    while True:
-        global check
-        #собирается id из 18 рандомных чисел
-        for i in range(18):
-            id += str(random.randint(0, 9))
-            count += 1
-        if count == 18:
-            # id будет использовтся для user id, сейчас находится мое id
-            await ctx.send(int(id))
-            count = 0
-            id = ""
-            check += 1
-            #будет использоваться переменная id для отправки сообщения user
-            user = await bot.fetch_user(user_id=855320640008617984)
-            # используеют глобальную переменную для счета созданных id, но отправляет их юзеру id которого было только что создано
-            #для удобства стоит испльзовать 2 вывода
-            await user.send(check)
 
 
 bot.run(TOKEN)
