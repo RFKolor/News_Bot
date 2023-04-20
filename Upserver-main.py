aae43baa0eb5e91f38ef449b9f986aee73c50bee
RFKolor <kiRiLLBaBasKiN909@yandex.ru>
2023-04-20 12:01:20 +0300

    немного функций

--- Upserver-main.py
+++ Upserver-main.py
@@ -15,14 +15,13 @@ bot = commands.Bot(command_prefix=settings.PREFIX, intents=intents,
 async def on_ready():
    print("бот работает")
 
+
 #вообще-то оно работать должно
 @client.event
 async def on_message_join(member):
     channel = client.get_channel(1096361181574340649)
-    embed=discord.Embed(title=f"Welcome {member.name}",
-                        description=f"Thanks for joining {member.guild.name}!")
+    embed=discord.Embed(title=f"Welcome {member.name}")
     embed.set_thumbnail(url=member.avatar_url)
-
     await channel.send(embed=embed)
 
 
@@ -49,6 +48,35 @@ async def flip(ctx):
 @bot.command(name='hello', description="Бот приветсвует вас")
 async def hello(ctx):
     await ctx.send(f"Привет {ctx.author.name}!")
+
+
+@bot.command(name="link", description="Ссылка на наш сайт")
+async def link(ctx):
+    embed = discord.Embed(
+        title=">---сслыка---<",
+        url="https://ya.ru/",
+        description="ссылка на наш сайт",
+        color=discord.Color.random()
+    )
+    await ctx.send(embed=embed)
+
+
+@bot.command(name="authors", description="ссылки на аккаунты разабочтиков бота и сайта")
+async def authors(ctx):
+    embed1 = discord.Embed(
+        title="Бот",
+        url="https://discordapp.com/users/855320640008617984/",
+        description="разработчик бота",
+        color=discord.Color.gold()
+        )
+    embed2 = discord.Embed(
+        title="Сайт",
+        url="https://discordapp.com/users/374813384060829697/",
+        description="разработчик сайта",
+        color=discord.Color.gold()
+    )
+    await ctx.send(embed=embed1)
+    await ctx.send(embed=embed2)
 
 
 bot.run(settings.TOKEN)
